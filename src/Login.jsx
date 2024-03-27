254bd61aa2bd23aac4182e0486f3961512235b99
simhadrimallaiah1998 <simhadrimallaiah1998@gmail.com>
2024-03-27 18:44:30 +0530

    implementing

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const changeToPage = () => {
-    console.log("This is clicked");
+  const [name, setName] = useState("");
+  const nameOfThePerson = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
   };
   return (
-    <div className="h-screen bg-blue-950 w-full flex flex-col justify-center items-center">
-      <h1 className="text-white text-3xl font-serif font-extrabold">
-        Welcome To The Movie Application
-      </h1>
-      <Link to="/movie">
-        <button
-          className="bg-white py-2 px-6 text-black font-bold"
-          onClick={changeToPage}
-        >
-          Click Me..!
-        </button>
-      </Link>
+    <div className="h-screen bg-[url('https://e1.pxfuel.com/desktop-wallpaper/72/1010/desktop-wallpaper-avatar-movie-for-laptop-avatar-movie-computer.jpg')] bg-contain w-full flex flex-col justify-center items-center">
+      <div className="grid grid-cols-3 w-full">
+        <div className="col-span-1 px-24">
+          <h1 className="text-white px-2 font-extrabold">
+            Welcome To The Movie World
+          </h1>
+          <input
+            className="w-full px-2 py-2 font-extrabold m-2 "
+            placeholder="Enter Your Name"
+            onChange={nameOfThePerson}
+          />
+          <Link to={`/movie/${name}`}>
+            <button className="bg-white py-2  m-2 px-6 text-black font-bold">
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="h-screen w-full">
+      <img
+        src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg"
+        alt="Not Found page"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
